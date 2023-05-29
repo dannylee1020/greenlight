c69ef95bff87d6ff347ad234252d000101c8a6cf
Daniel Lee <daniel.lee@circleci.com>
2023-05-28 23:53:55 -0700

    implement authentication

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -1,14 +1,68 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"net/http"
+	"strings"
 	"sync"
 	"time"
 
+	"greenlight.daniellee/internal/data"
+	"greenlight.daniellee/internal/validator"
+
 	"golang.org/x/time/rate"
 )
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// This indicates to any caches that the response may vary
+		// based on the value of the Authornization header in the request
+		w.Header().Add("Vary", "Authorization")
+
+		// returns empty string if not exist
+		authorizationHeader := r.Header.Get("Authorization")
+
+		if authorizationHeader == "" {
+			r = app.contextSetUser(r, data.AnonymousUser)
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		// we expect the value of the header to be in the format
+		// of Bearer <token>. If header isn't in this format
+		// return 401 Unauthorized response
+		headerParts := strings.Split(authorizationHeader, " ")
+		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+			app.invalidAuthenticationTokenResponse(w, r)
+			return
+		}
+
+		token := headerParts[1]
+
+		v := validator.New()
+		if data.ValidateTokenPlaintext(v, token); !v.Valid() {
+			app.invalidAuthenticationTokenResponse(w, r)
+			return
+		}
+
+		// retrieve user information
+		user, err := app.models.Tokens.GetForToken(data.ScopeAuthentication, token)
+		if err != nil {
+			switch {
+			case errors.Is(err, data.ErrRecordNotFound):
+				app.invalidAuthenticationTokenResponse(w, r)
+			default:
+				app.serverErrorResponse(w, r, err)
+			}
+			return
+		}
+
+		r = app.contextSetUser(r, user)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
